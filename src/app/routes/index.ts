51d375837ff83509c699a3c47e8501e2164136a9
Imtiaz Samir <imtiazsamir91@gmail.com>
2026-05-04 22:07:37 +0600

    create-doctor

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { SpecialtyRouter } from "../module/specialty/specialty.route";
 import { AuthRoutes } from "../module/auth/auth.route";
+import { UserRoute } from "../module/user/user.route";
 //import { SpecialtyRouter } from "../lib/module/specialty/specialty.route";
 
 
 const router = Router();
 router.use("/auth", AuthRoutes);
 router.use("/specialties", SpecialtyRouter);
+router.use("/users", UserRoute);
 
 
 export const indexRouter = router;
