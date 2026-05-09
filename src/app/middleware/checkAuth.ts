09bda89985f702e8ef5450634f91c970d7b86e43
Imtiaz Samir <imtiazsamir91@gmail.com>
2026-05-09 11:57:02 +0600

    getMe

--- src/app/middleware/checkAuth.ts
+++ src/app/middleware/checkAuth.ts
@@ -62,11 +62,11 @@ export const checkAuth = (...authRoles: Role[]) => async (req: Request, res: Res
                     throw new AppError(status.FORBIDDEN, 'Forbidden access! You do not have permission to access this resource.');
                 }
 
-                // req.user = {
-                //     userId : user.id,
-                //     role : user.role,
-                //     email : user.email,
-                // }
+                req.user = {
+                    userId : user.id,
+                    role : user.role,
+                    email : user.email,
+                }
             }
 
             const accessToken = CookieUtils.getCookie(req, 'accessToken');
--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -54,8 +54,22 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
         }
     });
 });
+const getMe = catchAsync(
+    async (req: Request, res: Response) => {
+        const user = req.user;
+        console.log({user});
+        const result = await AuthService.getMe(user);
+        sendResponse(res, {
+            httpStatusCode: status.OK,
+            success: true,
+            message: "User profile fetched successfully",
+            data: result,
+        })
+    }
+)
 
 export const AuthController = {
     registerPatient,
-    loginUser
+    loginUser,
+    getMe
 }
