80bd9c87c3e97d6fa7d9cc37279cff936ad01195
Imtiaz Samir <imtiazsamir91@gmail.com>
2026-05-09 10:40:08 +0600

    inded.d

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,7 +2,8 @@ import { Router } from "express";
 import { SpecialtyRouter } from "../module/specialty/specialty.route";
 import { AuthRoutes } from "../module/auth/auth.route";
 import { UserRoute } from "../module/user/user.route";
-import { DoctorRoute } from "../module/doctor/doctor.route";
+import { DoctorRoutes } from "../module/doctor/doctor.route";
+// import { DoctorRoute } from "../module/doctor/doctor.route";
 //import { SpecialtyRouter } from "../lib/module/specialty/specialty.route";
 
 
@@ -10,7 +11,7 @@ const router = Router();
 router.use("/auth", AuthRoutes);
 router.use("/specialties", SpecialtyRouter);
 router.use("/users", UserRoute);
-router.use("/doctors", DoctorRoute);
+router.use("/doctors", DoctorRoutes);
 
 
 export const indexRouter = router;
