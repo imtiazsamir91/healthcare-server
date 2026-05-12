7640882d9f61b07db2359ac4faa72600cd7f85b8
Imtiaz Samir <imtiazsamir91@gmail.com>
2026-05-12 10:10:58 +0600

    addRoute

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,8 @@ import { SpecialtyRouter } from "../module/specialty/specialty.route";
 import { AuthRoutes } from "../module/auth/auth.route";
 import { UserRoute } from "../module/user/user.route";
 import { DoctorRoutes } from "../module/doctor/doctor.route";
+import { AdminRoutes } from "../module/admin/admin.route";
+import { scheduleRoutes } from "../module/schedule/schedule.route";
 // import { DoctorRoute } from "../module/doctor/doctor.route";
 //import { SpecialtyRouter } from "../lib/module/specialty/specialty.route";
 
@@ -12,6 +14,10 @@ router.use("/auth", AuthRoutes);
 router.use("/specialties", SpecialtyRouter);
 router.use("/users", UserRoute);
 router.use("/doctors", DoctorRoutes);
+router.use("/admins", AdminRoutes)
+router.use("/schedules", scheduleRoutes)
+// router.use("/doctor-schedules", DoctorScheduleRoutes)
+// router.use("/appointments", AppointmentRoutes)
 
 
 export const indexRouter = router;
